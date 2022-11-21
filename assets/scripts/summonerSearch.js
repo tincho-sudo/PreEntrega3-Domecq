fa7325f492b61c2cae91d428a001c9a230fd15cb
tincho-sudo <martin8803@gmail.com>
2022-11-21 20:00:32 -0300

    js now checks if any box is empty (input or select) and lets you know modifying its border size and color

--- assets/scripts/summonerSearch.js
+++ assets/scripts/summonerSearch.js
@@ -1,15 +1,45 @@
 let summName = "";
 let region = "";
 let url = "";
+let opGgHeader = "";
+let yourUrl = "";
 
 const summonerSearchButton = document.getElementById("summonerSearch");
+const regionSelect = document.getElementById("autoSizingSelect");
+const summNameInput = document.getElementById("autoSizingInput");
 
 if (summonerSearchButton)
   summonerSearchButton.addEventListener("click", () => {
     region = document.getElementById("autoSizingSelect").value;
     summName = document.getElementById("autoSizingInput").value;
-    url = window.open(
-      "https://www.op.gg/summoners/" + region + "/" + summName,
-      "_blank"
-    );
+    if (region.length > 0 && summName.length > 0) {
+      url = "https://www.op.gg/summoners/" + region + "/" + summName;
+      window.open(url, "_blank");
+
+      regionSelect.style.borderColor = "#ced4da";
+      regionSelect.style.borderWidth = "1px";
+      summNameInput.style.borderColor = "#ced4da";
+      summNameInput.style.borderWidth = "1px";
+    } else if (region.length <= 0 && summName.length <= 0) {
+      regionSelect.style.borderColor = "red";
+      regionSelect.style.borderWidth = "15px";
+      summNameInput.style.borderColor = "red";
+      summNameInput.style.borderWidth = "15px";
+    } else if (region.length <= 0) {
+      regionSelect.style.borderColor = "red";
+      regionSelect.style.borderWidth = "15px";
+
+      if (summName.length > 0) {
+        summNameInput.style.borderColor = "#ced4da";
+        summNameInput.style.borderWidth = "1px";
+      }
+    } else {
+      summNameInput.style.borderColor = "red";
+      summNameInput.style.borderWidth = "15px";
+
+      if (region.length > 0) {
+        regionSelect.style.borderColor = "#ced4da";
+        regionSelect.style.borderWidth = "1px";
+      }
+    }
   });
