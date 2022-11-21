5d25fc4ba94642a244b79ce3ef2ff63a7ce532f8
tincho-sudo <martin8803@gmail.com>
2022-11-21 16:47:35 -0300

    js refactoring

--- assets/scripts/summonerSearch.js
+++ assets/scripts/summonerSearch.js
@@ -2,11 +2,13 @@ let summName = "";
 let region = "";
 let url = "";
 
-document.getElementById("summonerSearch").onclick = () => {
-  region = document.getElementById("autoSizingSelect").value;
-  summName = document.getElementById("autoSizingInput").value;
-  url = window.open(
-    "https://www.op.gg/summoners/" + region + "/" + summName,
-    "_blank"
-  );
-};
+summonerSearchButton = document.getElementById("summonerSearch");
+if (summonerSearchButton)
+  summonerSearchButton.addEventListener("click", () => {
+    region = document.getElementById("autoSizingSelect").value;
+    summName = document.getElementById("autoSizingInput").value;
+    url = window.open(
+      "https://www.op.gg/summoners/" + region + "/" + summName,
+      "_blank"
+    );
+  });
